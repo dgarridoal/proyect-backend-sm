79c4a398422c7e1de6281770c7425137ea2787d0
Diego Garrido <diego.garrido1801@alumnos.ubiobio.cl>
2022-05-17 16:44:49 -0400

    Rutas básicas iniciadas

--- APP/Routes/Auth.js
+++ APP/Routes/Auth.js
@@ -1,7 +1,44 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
+/**
+ * @api {post} /auth/login Login
+ * @apiName Login
+ * @apiGroup Auth
+ * @apiVersion  1.0.0
+ * @apiDescription Login
+ * @apiParam {String} email Email
+ * @apiParam {String} password Password
+ */
+router.post("/login", (req, res) => {
+  res.json("Auth login");
+});
 
+/**
+ * @api {post} /auth/register Register
+ * @apiName Register
+ * @apiGroup Auth
+ * @apiVersion  1.0.0
+ * @apiDescription Register
+ * @apiParam {String} email Email
+ * @apiParam {String} password Password
+ */
+router.post("/register", (req, res) => {
+  res.json("Auth register");
+});
 
+/**
+ * @api {post} /auth/change-password Change Password
+ * @apiName ChagePassword
+ * @apiGroup Auth
+ * @apiVersion  1.0.0
+ * @apiDescription Change Password
+ * @apiParam {String} email Email
+ * @apiParam {String} password Password
+ * @apiParam {String} new-password Password
+ */
+router.post("/change-password", (req, res) => {
+  res.json("Auth change password");
+});
 
 module.exports = router;
