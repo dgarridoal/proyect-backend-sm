c7e7b1fc34131013e090a67dcaf42acba594a689
Diego Garrido <diego.garrido1801@alumnos.ubiobio.cl>
2022-05-18 02:41:23 -0400

    Cambios de middlewares

--- APP/middlewares/validar-jwt.js
+++ APP/middlewares/validar-jwt.js
@@ -1,27 +1,25 @@
-const jwt=require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 const validarJWT = (req, res, next) => {
-
-    const token = req.header('x-token');
+  try {
+    const token = req.header("x-token");
     if (!token) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'No hay token en la petición'
-        });
-    }
-    try {
-        const {uid}=jwt.verify(token, process.env.JWT_SECRET);
-        req.uid=uid;
-        
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Token no válido'
-        });
+      return res.status(401).json({
+        ok: false,
+        msg: "No hay token en la petición",
+      });
     }
 
-}
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+    req.id = id;
 
-module.exports = {validarJWT};
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token no válido",
+    });
+  }
+};
+
+module.exports = { validarJWT };
