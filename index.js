7726ad8c1fa69cb04e561c1f75ca4a441f046272
Diego Garrido <diego.garrido1801@alumnos.ubiobio.cl>
2022-05-31 20:40:37 -0400

    FIXED Routes

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const cors=require('cors');
 
 const app = express();
 const PORT=process.env.PORT || 3000;
-const {bdConnect}=require('./Config/database');
+const {bdConnect}=require('./config/database');
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(express.static(__dirname + ''));
-app.use(express.static('APP'));
+app.use(express.static('src'));
 
 
 bdConnect();
-app.use('/api',require('./APP/Routes/index.js'));
+app.use('/api',require('./src/routes/index.js'));
 
 
 app.listen(PORT,()=>{
