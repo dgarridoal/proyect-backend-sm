0fc685ed1b2d3724415f6a4504c77e9251d60beb
Diego Garrido <diego.garrido1801@alumnos.ubiobio.cl>
2022-06-12 21:04:38 -0400

    FIXED index

--- src/routes/search.js
+++ src/routes/search.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { validarJWT } = require("../middlewares/validar-jwt");
-const { search, searchAll } =require("../controllers/Search");
+const { search, searchAll } =require("../controllers/search");
 
 /**
  * Route: /search
