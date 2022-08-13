0c047f9e954882d25912e0fe8951adba8512c3de
Diego Garrido <diego.garrido1801@alumnos.ubiobio.cl>
2022-08-13 17:00:06 -0400

    Update user implementado

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,11 +1,12 @@
 const userModel = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const updateUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const { body } = req;
+    const { nombre, apellido, email, password,confirmPassword, newPassword } = req.body;
 
-    const user = await userModel.findByIdAndUpdate(id, body, { new: true });
+    const user = await userModel.findById(id);
 
     if (!user) {
       return res.status(400).json({
@@ -14,14 +15,54 @@ const updateUser = async (req, res) => {
       });
     }
 
-    return res.status(200).json({
-      status: true,
-      user,
-    });
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+    
+    if (!validPassword) {
+      return res.status(400).json({
+        status: false,
+        message: "La contraseña es incorrecta",
+      });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        status: false,
+        message: "Las contraseñas no coinciden",
+      });
+    }
+
+    if(!newPassword){
+      user.nombre = nombre;
+      user.apellido = apellido;
+      user.email = email;
+
+      const userUpdate = await userModel.findByIdAndUpdate(id, user, { new: true });
+
+      return res.status(200).json({
+        status: true,
+        user: userUpdate,
+      });
+
+    }else{
+      const salt = bcrypt.genSaltSync(10);
+      const newPasswordHash = bcrypt.hashSync(newPassword, salt);
+      user.nombre = nombre;
+      user.apellido = apellido;
+      user.email = email;
+      user.password = newPasswordHash;
+
+      const userUpdate = await userModel.findByIdAndUpdate(id, user, { new: true });
+
+      return res.status(200).json({
+        status: true,
+        user: userUpdate,
+      });
+    }
+  
   } catch (error) {
     return res.status(500).json({
       status: false,
-      message: 'Error al actualizar el usuario',
+      message: "Error al actualizar el usuario",
     });
   }
 };
--- src/routes/User.js
+++ src/routes/User.js
@@ -8,6 +8,6 @@ const { updateUser } = require("../controllers/User");
  * Route: /user
  */
 
-router.put("update/:id", [validarJWT], updateUser);
+router.put("/update/:id", [validarJWT], updateUser);
 
 module.exports = router;
