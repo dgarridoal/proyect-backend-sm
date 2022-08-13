be947f4d0d440e738da101e81214841ac991366f
Diego Garrido <diego.garrido1801@alumnos.ubiobio.cl>
2022-08-13 17:25:38 -0400

    JWT User

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,10 +1,12 @@
 const userModel = require("../models/User");
 const bcrypt = require("bcryptjs");
+const { generarJwt } = require("../helpers/jwt");
 
 const updateUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const { nombre, apellido, email, password,confirmPassword, newPassword } = req.body;
+    const { nombre, apellido, email, password, confirmPassword, newPassword } =
+      req.body;
 
     const user = await userModel.findById(id);
 
@@ -15,9 +17,8 @@ const updateUser = async (req, res) => {
       });
     }
 
-
     const validPassword = bcrypt.compareSync(password, user.password);
-    
+
     if (!validPassword) {
       return res.status(400).json({
         status: false,
@@ -31,19 +32,20 @@ const updateUser = async (req, res) => {
       });
     }
 
-    if(!newPassword){
+    if (!newPassword) {
       user.nombre = nombre;
       user.apellido = apellido;
       user.email = email;
 
-      const userUpdate = await userModel.findByIdAndUpdate(id, user, { new: true });
+      const userUpdate = await userModel.findByIdAndUpdate(id, user, {
+        new: true,
+      });
 
       return res.status(200).json({
         status: true,
         user: userUpdate,
       });
-
-    }else{
+    } else {
       const salt = bcrypt.genSaltSync(10);
       const newPasswordHash = bcrypt.hashSync(newPassword, salt);
       user.nombre = nombre;
@@ -51,14 +53,17 @@ const updateUser = async (req, res) => {
       user.email = email;
       user.password = newPasswordHash;
 
-      const userUpdate = await userModel.findByIdAndUpdate(id, user, { new: true });
+      const userUpdate = await userModel.findByIdAndUpdate(id, user, {
+        new: true,
+      });
+      const token = await generarJwt(user.id);
 
       return res.status(200).json({
         status: true,
         user: userUpdate,
+        token
       });
     }
-  
   } catch (error) {
     return res.status(500).json({
       status: false,
